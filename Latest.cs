ee26b60bcb23a58a68bfd1658d8c03504744b957
Ajay Bhat <ajaybhat777@gmail.com>
2023-03-15 15:36:14 +0530

    Update Latest.cs

--- Latest.cs
+++ Latest.cs
@@ -13,18 +13,24 @@ namespace SharePointOnlineGraphApi
         static async Task Main(string[] args)
         {
             var clientId = "your-client-id";
+            var clientSecret = "your-client-secret";
             var tenantId = "your-tenant-id";
 
-            var clientApplication = PublicClientApplicationBuilder
+            var clientApplication = ConfidentialClientApplicationBuilder
                 .Create(clientId)
+                .WithClientSecret(clientSecret)
                 .WithAuthority($"https://login.microsoftonline.com/{tenantId}")
                 .Build();
 
             var scopes = new[] { "https://graph.microsoft.com/.default" };
-            var authenticationResult = await clientApplication.AcquireTokenInteractive(scopes).ExecuteAsync();
+            var authenticationResult = await clientApplication.AcquireTokenForClient(scopes).ExecuteAsync();
             var accessToken = authenticationResult.AccessToken;
 
-            var authProvider = new TokenCredentialAuthProvider(accessToken);
+            var authProvider = new DelegateAuthenticationProvider(async (requestMessage) =>
+            {
+                requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            });
+
             var graphClient = new GraphServiceClient(authProvider, new[] { new JsonSerializationWriterFactory(), new JsonSerializationReaderFactory() });
 
             var listId = "your-list-id";
