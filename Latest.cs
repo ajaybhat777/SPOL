cb2b664cea73400455e572b3d249a961cf50acc6
Ajay Bhat <ajaybhat777@gmail.com>
2023-03-15 16:25:55 +0530

    Update Latest.cs

--- Latest.cs
+++ Latest.cs
@@ -1,45 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Graph;
 using Microsoft.Graph.Auth;
 using Microsoft.Identity.Client;
-using Microsoft.Kiota.Abstractions.Serialization;
-using Microsoft.Kiota.Serialization.Json;
-using System;
-using System.Threading.Tasks;
 
-namespace SharePointOnlineGraphApi
+namespace GraphApiExample
 {
     class Program
     {
         static async Task Main(string[] args)
         {
-            var clientId = "your-client-id";
-            var clientSecret = "your-client-secret";
-            var tenantId = "your-tenant-id";
+            var clientId = "YOUR_CLIENT_ID";
+            var clientSecret = "YOUR_CLIENT_SECRET";
+            var tenantId = "YOUR_TENANT_ID";
 
-            var clientApplication = ConfidentialClientApplicationBuilder
+            var confidentialClientApplication = ConfidentialClientApplicationBuilder
                 .Create(clientId)
+                .WithTenantId(tenantId)
                 .WithClientSecret(clientSecret)
-                .WithAuthority($"https://login.microsoftonline.com/{tenantId}")
                 .Build();
 
             var scopes = new[] { "https://graph.microsoft.com/.default" };
-            var authenticationResult = await clientApplication.AcquireTokenForClient(scopes).ExecuteAsync();
-            var accessToken = authenticationResult.AccessToken;
 
-            var authProvider = new DelegateAuthenticationProvider(async (requestMessage) =>
+            var authenticationProvider = new ClientCredentialProvider(confidentialClientApplication, scopes);
+
+            var httpClient = new HttpClient();
+            var httpMessageHandler = new AuthHandler(authenticationProvider, httpClient);
+            var graphClient = new GraphServiceClient(httpMessageHandler);
+
+            var users = await graphClient.Users.Request().GetAsync();
+
+            foreach (var user in users)
             {
-                requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
-            });
-
-            var graphClient = new GraphServiceClient(authProvider, new[] { new JsonSerializationWriterFactory(), new JsonSerializationReaderFactory() });
-
-            var listId = "your-list-id";
-            var items = await graphClient.Sites["root"].Lists[listId].Items.Request().GetAsync();
-
-            foreach (var item in items)
-            {
-                Console.WriteLine(item.Id);
+                Console.WriteLine(user.DisplayName);
             }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class AuthHandler : HttpMessageHandler
+    {
+        private readonly IAuthenticationProvider _authenticationProvider;
+        private readonly HttpClient _httpClient;
+
+        public AuthHandler(IAuthenticationProvider authenticationProvider, HttpClient httpClient)
+        {
+            _authenticationProvider = authenticationProvider;
+            _httpClient = httpClient;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            await _authenticationProvider.AuthenticateRequestAsync(request);
+            return await _httpClient.SendAsync(request, cancellationToken);
         }
     }
 }
