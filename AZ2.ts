514f4722d560e2822d41cc257103a7436c587b46
Ajay Bhat <ajaybhat777@gmail.com>
2023-03-28 18:26:51 +0530

    Create AZ2.ts

---
+++ AZ2.ts
@@ -0,0 +1,49 @@
+interface InputObject {
+  PrimaryName: string;
+  Letter: string;
+  alternateLetter1: string;
+  alternateName1: string;
+  alternateLetter2: string;
+  alternateName2: string;
+  description: string;
+}
+
+interface OutputObject {
+  Title: string;
+  AlsoKnownAs: string;
+  Description: string;
+}
+
+function transformList(inputList: InputObject[], selectedLetter: string): OutputObject[] {
+  return inputList.flatMap((obj) => {
+    if (obj.Letter === selectedLetter) {
+      return {
+        Title: obj.PrimaryName,
+        AlsoKnownAs: '',
+        Description: obj.description,
+      };
+    }
+
+    if (obj.alternateLetter1 === selectedLetter) {
+      return [
+        {
+          Title: obj.alternateName1,
+          AlsoKnownAs: obj.PrimaryName,
+          Description: obj.description,
+        },
+      ];
+    }
+
+    if (obj.alternateLetter2 === selectedLetter) {
+      return [
+        {
+          Title: obj.alternateName2,
+          AlsoKnownAs: obj.PrimaryName,
+          Description: obj.description,
+        },
+      ];
+    }
+
+    return [];
+  });
+}
